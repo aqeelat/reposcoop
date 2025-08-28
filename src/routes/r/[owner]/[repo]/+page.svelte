c154294c6a5ae800484ad257750ac47b16c97403
Abdullah Alaqeel <abdullah.t.aqeel@gmail.com>
2025-08-28 12:59:13 +0100

    chore: fix route param typing

--- src/routes/r/[owner]/[repo]/+page.svelte
+++ src/routes/r/[owner]/[repo]/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { page } from '$app/state';
   import RepoPage from '$lib/components/repo/RepoPage.svelte';
-  const owner = $derived(page.params.owner);
-  const repo = $derived(page.params.repo);
+  const owner = $derived(page.params.owner!);
+  const repo = $derived(page.params.repo!);
 </script>
 
 <RepoPage {owner} {repo} />
